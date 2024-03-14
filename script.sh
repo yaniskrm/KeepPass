b08ef336bac903f394028521f4ea3cc2b5c92ae4
averrani <aris.issad@gmail.com>
2024-03-14 14:56:05 +0100

    script update

--- script.sh
+++ script.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-# Lancer nodemon pour le back-end
-xterm -e 'bash -c "npx nodemon back/index.js; exec bash"' &
+# Fonction pour lancer nodemon pour le back-end
+launch_backend() {
+  if [ "$(uname)" == "Darwin" ]; then
+    # Pour macOS
+    osascript -e 'tell app "Terminal" to do script "npx nodemon back/index.js"'
+  else
+    # Pour Linux
+    xterm -e 'bash -c "npx nodemon back/index.js; exec bash"' &
+  fi
+}
 
-# Lancer nodemon pour le front-end
-xterm -e 'bash -c "npx nodemon front/index.js; exec bash"' &
+# Fonction pour lancer nodemon pour le front-end
+launch_frontend() {
+  if [ "$(uname)" == "Darwin" ]; then
+    # Pour macOS
+    osascript -e 'tell app "Terminal" to do script "npx nodemon front/index.js"'
+  else
+    # Pour Linux
+    xterm -e 'bash -c "npx nodemon front/index.js; exec bash"' &
+  fi
+}
+
+# Exécuter les fonctions en arrière-plan
+launch_backend &
+launch_frontend &
